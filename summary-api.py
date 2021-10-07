d9d5e93cc3e40e0424aa3d1852fee9aed9840d05
Uğur KARAASLAN <62725213+uk3435@users.noreply.github.com>
2021-10-07 09:46:00 +0300

    Update summary-api.py

--- summary-api.py
+++ summary-api.py
@@ -1,4 +1,4 @@
-import os
+#import os
 from flask import Flask,request
 #import openai
 #import json
@@ -50,7 +50,7 @@ def api():
     return str(summ)
     
 if __name__ =="__main__":
-    port=int(os.environ.get("PORT",5000))
+    port=5000
     app.run(host='0.0.0.0',port=port)
 
 
